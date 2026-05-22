615e1efe4f2eda13a9c64835b8fdd9439f74c89f
DP <dylan.pine115@gmail.com>
2026-05-21 19:06:01 -0600

    List Creation
    Added the ability to enter values and display a list showing the values entered

--- list.js
+++ list.js
@@ -1,11 +1,21 @@
 let list =[], complete =[], incomplete =[];
-
 const task = document.getElementById("myInput");
 
-task.addEventListener('keydown', function search(event){
-    if (event.key === 'Enter') {
+function addToList(task) {
+    const newItem = document.createElement('li');
+    const containter = document.getElementById('todo-list')
+    
+    newItem.innerText = task;
+    newItem.className = 'todo-item';
+    containter.appendChild(newItem);
+
+}
+task.addEventListener('keyup', function search(event){
+    if (event.key === 'Enter' && task.value != '') {
         list.push(task.value);
-        console.log(list);
+        addToList(task.value);
         task.value = "";
+        //document.getElementById("display-area").innerHTML = task.value;
+        
     }
 })
